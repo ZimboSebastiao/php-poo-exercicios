55133dabc843e121c2876f823f1cb1e6e5dc0996
Zimbo Sebastião <gloriasebastiao7@gmail.com>
2023-09-22 11:50:57 -0300

    Remoção dos getters setters

--- src/Livro.php
+++ src/Livro.php
@@ -5,33 +5,7 @@ class Livro {
     private string $autor;
     private int $paginas = 0;
 
-    // TITULO
-    public function setTitulo(string $titulo):void {
-        $this->titulo = $titulo;
-    }
-
-    public function getTitulo():string {
-       return $this->titulo;
-    }
-
-    // AUTOR
-    public function setAutor(string $autor):void {
-        $this->autor = $autor;
-    }
-
-    public function getAutor():string {
-       return $this->autor;
-    }
-
-    // PAGINAS
-    public function setPaginas(string $paginas):void {
-        $this->paginas = $paginas;
-    }
-
-    public function getPaginas():string {
-       return $this->paginas;
-    }
-
+  
 }
 
 
